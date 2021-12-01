dbb11dbc87a156f9d8b304d8a6daf3e6c8d9cd86
Rui Freitas <rui.freitas@hopin.to>
2021-12-01 14:08:23 +0000

    Fix python

--- python/day_1/sonar.py
+++ python/day_1/sonar.py
@@ -1,5 +1,22 @@
 # pylint: disable=missing-module-docstring
 
+import os
+
+def main():
+  dirname = os.path.dirname(__file__)
+  filename = os.path.join(dirname, '../../data/day_1/input.txt')
+
+  with open(filename) as f:
+    inputs = f.read().splitlines()
+
+  inputs = list(map(int, inputs))
+
+  result =  measurement_increases(inputs)
+  window = sliding_window_increases(inputs)
+
+  print("Increases: {} | Window: {}".format(result, window))
+
+
 def measurement_increases(inputs):
   counter = 0
   for index, input in enumerate(inputs):
@@ -11,5 +28,8 @@ def sliding_window_increases(inputs):
   sums = []
   for index, input in enumerate(inputs[:-2]):
     sums.append(input + inputs[index+1] + inputs[index+2])
-  print(sums)
+
   return measurement_increases(sums)
+
+if __name__ == '__main__':
+    main()
