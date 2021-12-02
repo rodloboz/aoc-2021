83b2cd2aa1c038a1e1f422c00269af83d35045d6
Rui Freitas <dinnerpartysaboteur@gmail.com>
2021-12-02 11:41:29 +0000

    Delete rust/day_1/target directory

--- rust/day_1/target/rls/debug/deps/sonar-0bbc6eff154ebc87.d
+++
@@ -1,5 +0,0 @@
-/Users/ruifreitas/code/aoc/rust/day_1/target/rls/debug/deps/sonar-0bbc6eff154ebc87.rmeta: src/main.rs
-
-/Users/ruifreitas/code/aoc/rust/day_1/target/rls/debug/deps/sonar-0bbc6eff154ebc87.d: src/main.rs
-
-src/main.rs:
