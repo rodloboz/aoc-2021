2878e19f15f19a7c74ec5703ada17c236e9c82e7
Rui Freitas <rui.freitas@hopin.to>
2021-12-03 13:07:57 +0000

    Solved Day3 - Part1 in Typescript

---
+++ typescript/src/day_3/__tests__/diagnostic.test.ts
@@ -0,0 +1,21 @@
+import { calculatePowerConsumption } from '../diagnostic';
+
+describe('calculatePowerConsumption', () => {
+  it('returns the power consumption of the submarine', () => {
+    const inputs = [
+      '00100',
+      '11110',
+      '10110',
+      '10111',
+      '10101',
+      '01111',
+      '00111',
+      '11100',
+      '10000',
+      '11001',
+      '00010',
+      '01010'
+    ]
+    expect(calculatePowerConsumption(inputs)).toEqual(198)
+  })
+});
---
+++ typescript/src/day_3/diagnostic.ts
@@ -0,0 +1,39 @@
+export const calculatePowerConsumption = (inputs: string[]) : number => {
+  const bits: Array<string[]> = inputs.map(input => Array.from(input));
+  return gammaRate(bits) * epsilonRate(bits);
+}
+
+const gammaRate = (bits: Array<string[]>): number => {
+  const bitsArr: string[] = []
+  for(let i = 0; i < bits[0].length; i++) {
+    const column = bits.map(row => row[i])
+    bitsArr.push(mostCommonBit(column))
+  }
+  return parseInt( bitsArr.join(''), 2 );
+}
+
+const epsilonRate = (bits: Array<string[]>): number => {
+  const bitsArr: string[] = []
+  for(let i = 0; i < bits[0].length; i++) {
+    const column = bits.map(row => row[i])
+    bitsArr.push(mostCommonBit(column) == '1' ? '0' : '1')
+  }
+  return parseInt( bitsArr.join(''), 2 );
+}
+
+const mostCommonBit = (bits: string[], tiebreaker = '1'): string => {
+  const modeMap = new Map();
+  let mostCommon = bits[0], maxCount = 1;
+  for(let i = 0; i < bits.length; i++) {
+    let bit = bits[i];
+    let count = modeMap.get(bit);
+    modeMap.set(bit, count ? count + 1 : 1)
+
+    count = modeMap.get(bit);
+    if(count > maxCount || (count == maxCount && bit == tiebreaker)) {
+        mostCommon = bit;
+        maxCount = modeMap.get(bit);
+    }
+  }
+  return mostCommon;
+}
