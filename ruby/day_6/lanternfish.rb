2833ac803b5fa4544c566ba3eee1c9591ad8c4f5
Rui Freitas <rui.freitas@hopin.to>
2021-12-06 06:44:04 +0000

    Add timer constants

--- ruby/day_6/lanternfish.rb
+++ ruby/day_6/lanternfish.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 class Lanternfish
+  TIMER_AFTER_RESPAWN = 6
+  TIMER_FOR_NEWBORNS = 8
+
   def self.new_school(initial_state)
     new(initial_state)
   end
@@ -21,8 +24,8 @@ class Lanternfish
       temp_map = Hash.new(0)
       school.each_key do |key|
         if key.zero?
-          temp_map[6] += school[0]
-          temp_map[8] += school[0]
+          temp_map[TIMER_AFTER_RESPAWN] += school[0]
+          temp_map[TIMER_FOR_NEWBORNS] += school[0]
         else
           temp_map[key - 1] += school[key]
         end
