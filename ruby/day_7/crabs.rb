2bf180cdb4ddb7737ff8b61e4c99bf7515e0fa3e
Rui Freitas <rui.freitas@hopin.to>
2021-12-07 08:35:27 +0000

    Use reducer instead of each

--- ruby/day_7/crabs.rb
+++ ruby/day_7/crabs.rb
@@ -3,21 +3,22 @@
 def fuel_cost(input)
   sorted = input.sort
   half = (sorted.size / 2.to_f).floor
-  min_l1(input, sorted[half])
+  total_cost(input, sorted[half])
 end
 
 def enhanced_fuel_cost(input)
   sorted = input.map { |n| n * (n + 1) }.sort
   half = (sorted.size / 2.to_f).floor
-  min_l1(input, half) { |moves| (moves * (moves + 1) / 2) }
+  total_cost(input, half) { |moves| (moves * (moves + 1) / 2) }
 end
 
-def min_l1(array, target)
-  cost = 0
-  array.each do |number|
+def total_cost(numbers, target)
+  numbers.reduce(0) do |cost, number|
     moves = (target - number).abs
-    cost += block_given? ? yield(moves) : moves
+    if block_given?
+      cost + yield(moves)
+    else
+      cost + moves
+    end
   end
-
-  cost
 end
